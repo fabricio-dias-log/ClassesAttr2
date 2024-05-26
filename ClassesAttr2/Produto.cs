5ad82e603b0b1dca9b12dddc4450063331c80c44
Fabricio Dias <fabricio270909@gmail.com>
2024-05-26 19:42:48 -0300

    feat: funções para adicionar, detalhar e remover produto

---
+++ ClassesAttr2/Produto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesAttr2
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Valor;
+        public int Quantidade;
+
+        public double ValorTotalEmEstoque()
+        {
+            return Quantidade * Valor;
+        }
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} | Preço: R${Valor.ToString("F2")} | Estoque: {Quantidade} | Total R$: R${ValorTotalEmEstoque().ToString("F2")}";
+        }
+
+        public void AdicionarProdutos(int quantity)
+        {
+            Quantidade += quantity;
+            ToString();
+        }
+
+        public void RemoverProdutos(int quantity)
+        {
+            Quantidade -= quantity;
+            ToString();
+        }
+    }
+}
--- ClassesAttr2/Program.cs
+++ ClassesAttr2/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace ClassesAttr2
+﻿using System.Globalization;
+
+namespace ClassesAttr2
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Produto produto = new Produto();
+            int option;
+            int qtd;
+
+            Console.WriteLine("Insira os dados do produto:");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+
+            Console.Write("Preço: ");
+            produto.Valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade: ");
+            produto.Quantidade = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("-----------------------------");
+                Console.WriteLine("Digite a opção desejada:");
+                Console.WriteLine("1 - Detalhes do produto");
+                Console.WriteLine("2 - Adicionar produto");
+                Console.WriteLine("3 - Remover produto");
+                Console.WriteLine("-----------------------------");
+                Console.Write("Opção: ");
+
+                option = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+
+                switch (option)
+                {
+                    case 1:
+                        Console.WriteLine($"Dados do produto -> {produto}");
+
+                        break;
+                    case 2:
+                        Console.Write("Digite a quantidade desejada: ");
+
+                        qtd = int.Parse(Console.ReadLine());
+                        produto.AdicionarProdutos(qtd);
+
+                        break;
+                    case 3:
+                        Console.Write("Digite a quantidade desejada: ");
+
+                        qtd = int.Parse(Console.ReadLine());
+                        produto.RemoverProdutos(qtd);
+
+                        break;
+                    default:
+                        return;
+                }
+
+            } while (option != 0);
         }
     }
 }
